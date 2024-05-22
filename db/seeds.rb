90df9b18614897be2ce684957ad80922ee1c6b37
Kenzo Defoy <kenzo.defoy@icloud.com>
2024-05-22 15:32:55 +0200

    Exo 2

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,18 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puts "Creating restaurants..."
+restaurants = [
+  { name: 'Le Gourmet', address: '123 Rue de Paris', phone_number: '01 23 45 67 89', category: 'french' },
+  { name: 'Sushi Master', address: '456 Tokyo Street', phone_number: '02 34 56 78 90', category: 'japanese' },
+  { name: 'Pasta Palace', address: '789 Roma Avenue', phone_number: '03 45 67 89 01', category: 'italian' },
+  { name: 'Chinatown Delight', address: '321 Beijing Road', phone_number: '04 56 78 90 12', category: 'chinese' },
+  { name: 'Belgian Bistro', address: '654 Brussels Square', phone_number: '05 67 89 01 23', category: 'belgian' }
+]
+
+restaurants.each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
